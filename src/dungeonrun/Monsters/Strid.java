b0c60efbc7a9e9d1766edce19bfe6154f26a1bab
anveden <73692385+anveden@users.noreply.github.com>
2020-11-05 11:45:22 +0100

    Strid update

--- src/dungeonrun/Monsters/Strid.java
+++ src/dungeonrun/Monsters/Strid.java
@@ -5,47 +5,39 @@ import java.util.InputMismatchException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Strid {
+public class Strid extends Creatures {
+    ArrayList<Integer> dice = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
     public void stridDice() {
         boolean programRunner = true;
-        System.out.println("\nNamn: Simon Anveden, Datum: 5/30/2020 \n");
-        Scanner input = new Scanner(System.in);
+
+
         while (programRunner) {
 
             try {
                 //Main input of program, dictates programRunner
-                System.out.print("\nSkriv 1 för att kasta, Skriv 0 för att avsluta: ");
+                System.out.print("\nInput '1' to throw dice or '0' to exit: ");
                 int mainInput = input.nextInt();
+
+
+
+
 
                 //if input == 1, proceed with dice
                 if (mainInput == 1) {
-                    ArrayList<Integer> dice = new ArrayList<>();
 
-                    System.out.print("Välj antal tärningskast mellan 1 och 5: ");
-                    int diceRolls = input.nextInt();
+                    //test creature with fixed initiative and attack
+                    initiative=3;
+                    attack=4;
 
-                    if (diceRolls > 5 || diceRolls < 1) {
-                        throw new Exception("Välj mellan 1 och 5 tärningskast, du valde: " + diceRolls);
-                    }
-                    int sum = 0;
-                    System.out.println();
-                    int totalRolls = diceRolls;
-                    for (int i = 1; i < totalRolls + 1; i++) {
-                        int roll = rollthedice();
-                        while (roll == 6) {
-                            System.out.println("Resultat Tärning nummer " + i + " är 6, kastar 2 nya tärningar ...");
-                            totalRolls += 2;
-                            i++;
-                            roll = rollthedice();
-                        }
-                        dice.add(roll);
-                        sum += roll;
-                        System.out.println("Resultat Tärning nummer " + i + ": " + roll + ", Summa: " + sum);
-                    }
+                    diceRoll(initiative);
+                    diceRoll(attack);
 
-                    System.out.println("\nSumma alla tärningar: " + sum);
-                    System.out.print("Totalt antal tärningskast: " + totalRolls + "\n");
 
+
+
+
+//felhantering och exit
 
                 } else if (mainInput == 0) {
                     //if input == 0, exit program
@@ -54,20 +46,39 @@ public class Strid {
 
                 //if input is neither 1 or 0, inform user of invalid format
                 else {
-                    System.out.print("Ogiltig inmatning, skriv 1 eller 0. Du skrev: " + mainInput + "\n");
+                    System.out.print("Incorrect input, enter '1' or '0'. You wrote: " + mainInput + "\n");
                 }
 
             } catch (Exception e) {
                 //Any corrupted or invalid input results in a exception. Inform user that the input is invalid
-                System.out.print("Ogiltig inmatning, Försök igen (Måste vara ett heltal)  ...\n");
+                System.out.print("Invalid input. Must be integers...\n");
                 System.out.println(e.getMessage() != null ? e.getMessage() : "");
+                input.next();
                 programRunner = true;
             }
         }
 
     }
-        public static int rollthedice () {
-            return (int) (Math.random() * 6) + 1;
+
+    public static int rollthedice() {
+        return (int) (Math.random() * 6) + 1;
+    }
+    public int diceRoll(int diceRolls){
+        System.out.print("Throwing dice!");
+        int sum = 0;
+        System.out.println();
+        for (int i = 1; i < diceRolls + 1; i++) {
+            int roll = rollthedice();
+
+            dice.add(roll);
+            sum += roll;
+            System.out.println("Result of dice number " + i + ": " + roll + ", Sum: " + sum);
         }
+
+        System.out.println("Sum of all dice: " + sum);
+        System.out.println("Total amount of throws: " + diceRolls + "\n");
+        return sum;
+
+    }
 
 }
