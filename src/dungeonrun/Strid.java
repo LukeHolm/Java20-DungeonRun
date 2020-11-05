b49e9e09a3c92c7d669d03519d73032b7374d62c
anveden <73692385+anveden@users.noreply.github.com>
2020-11-05 15:41:29 +0100

    Strid update

--- src/dungeonrun/Strid.java
+++ src/dungeonrun/Strid.java
@@ -1,13 +1,20 @@
 package dungeonrun;
+
+import dungeonrun.Characters.Knight;
 import dungeonrun.Monsters.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Strid extends Creatures{
+public class Strid {
     ArrayList<Integer> dice = new ArrayList<>();
     Scanner input = new Scanner(System.in);
-    Room randomMonster = new Room();
+    Room randomRoom = new Room();
+    Monster monsterObj = randomRoom.getNextMonster();
+    //temp adventurer
+    Knight simon = new Knight();
+
+
     public void stridDice() {
         boolean programRunner = true;
 
@@ -16,21 +23,48 @@ public class Strid extends Creatures{
 
             try {
                 //Main input of program, dictates programRunner
-                System.out.print("\nInput '1' to throw dice or '0' to exit: ");
+                System.out.print("\nTo attack press '1' to to run away press '0': ");
                 int mainInput = input.nextInt();
-
-
-
 
 
                 //if input == 1, proceed with dice
                 if (mainInput == 1) {
-                    System.out.println(randomMonster.getNextMonster().creatureIsA);
-                    int monsterAttack = randomMonster.getNextMonster().attack;
-                    System.out.println("Attack: " + monsterAttack);
-                    System.out.println(diceRoll(monsterAttack));
+                    System.out.println("You encounter a vicious " + monsterObj.creatureIsA + "!");
+                    boolean fight = true;
+                    while (fight) {
+                        int monsterAtk = diceRoll(monsterObj.attack);
+                        int simonDef = diceRoll(simon.agility);
 
 
+                        System.out.println("The " + monsterObj.creatureIsA + " attacks you for " + monsterAtk + " damage!");
+                        System.out.println("You defend yourself for " + simonDef);
+                        if (monsterAtk > simonDef) {
+
+                            System.out.println("You took damage! You had " + simon.toughness + " toughness");
+                            simon.toughness--;
+                            System.out.println("But now you have " + simon.toughness);
+
+                        }
+                        if (monsterAtk < simonDef) {
+                            System.out.println("You defended yourself from the attack!");
+                        } else if (monsterAtk == simonDef) {
+                            System.out.println("Draw!");
+                        }
+                        if (simon.toughness == 0 ) {
+                            System.out.println("----------------------------");
+                            System.out.println("Simon the brave knight died!");
+                            System.out.println("----------------------------");
+                            fight = false;
+                        }
+                        monsterObj.initiative--;
+                        if (monsterObj.initiative==0){
+                            System.out.println("------------");
+                            System.out.println("You made it!");
+                            System.out.println("------------");
+                            fight = false;
+                        }
+
+                    }
 
 //felhantering och exit
 
@@ -58,8 +92,9 @@ public class Strid extends Creatures{
     public static int rollthedice() {
         return (int) (Math.random() * 6) + 1;
     }
-    public int diceRoll(int diceRolls){
-        System.out.print("Throwing dice!");
+
+    public int diceRoll(int diceRolls) {
+        //System.out.print("Throwing dice!");
         int sum = 0;
         System.out.println();
         for (int i = 1; i < diceRolls + 1; i++) {
@@ -67,11 +102,11 @@ public class Strid extends Creatures{
 
             dice.add(roll);
             sum += roll;
-            System.out.println("Result of dice number " + i + ": " + roll + ", Sum: " + sum);
+            //System.out.println("Result of dice number " + i + ": " + roll + ", Sum: " + sum);
         }
 
-        System.out.println("Sum of all dice: " + sum);
-        System.out.println("Total amount of throws: " + diceRolls + "\n");
+        //System.out.println("Sum of all dice: " + sum);
+        //System.out.println("Total amount of throws: " + diceRolls + "\n");
         return sum;
 
     }
