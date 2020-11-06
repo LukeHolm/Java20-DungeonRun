c28822b6d523173a8a643867bc7557794afa9c8b
anveden <73692385+anveden@users.noreply.github.com>
2020-11-06 09:32:13 +0100

    Strid update

--- src/dungeonrun/Strid.java
+++ src/dungeonrun/Strid.java
@@ -31,11 +31,11 @@ public class Strid {
                     boolean fight = true;
                     while (fight) {
                         int simonAtk = diceRoll(simon.attack);
-                        int monsterDef = diceRoll(monsterObj.agility);                       
-                       
-                        System.out.println("You attack for " + simonAtk  + " damage!");
+                        int monsterDef = diceRoll(monsterObj.agility);
+
+                        System.out.println("You attack for " + simonAtk + " damage!");
                         System.out.println("The " + monsterObj.creatureIsA + " defends for " + monsterDef);
-                        
+
                         if (simonAtk > monsterDef) {
                             System.out.println("The monster tooks damage! The monster had " + monsterObj.toughness + " toughness");
                             monsterObj.toughness--;
@@ -45,85 +45,89 @@ public class Strid {
                         } else if (simonAtk == monsterDef) {
                             System.out.println("Draw!");
                         }
-                        
-                        if (monsterObj.toughness == 0 ) {
+
+                        if (monsterObj.toughness == 0) {
                             System.out.println("----------------------------");
                             System.out.println("The monster has been killed!");
                             System.out.println("----------------------------");
                             fight = false;
                             break;
                         }
-                        
+
                         simon.initiative--;
-                        if (simon.initiative==0){
+                        if (simon.initiative == 0) {
                             System.out.println("------------");
                             System.out.println("Simon the brave knight died!");
                             System.out.println("------------");
                             fight = false;
-                         }                         
+                        }
                     }
 
 //felhantering och exit
 
                 } else if (mainInput == 0) {
                     //if input == 0, exit program only when player's chance of escape ... 
-                    int chanceEscape = simon.agility * 10;
+                    double chanceEscape = simon.agility * 0.1;
+                    double escChance = Math.random();
+
                     // what to do with chanceEscape? compare it with what? when the player escapes?
-                    if (chanceEscape)
-                    {
+                    if (chanceEscape>escChance) {
+                        System.out.println("You escaped!");
                         programRunner = false;
                     } else {
-                    // monster attacks                         
-                    System.out.println("You faild to escape. The " + monsterObj.creatureIsA + " attacks!");
-                    boolean fight = true;
-                    while (fight) {
-                        int monsterAtk = diceRoll(monsterObj.attack);
-                        int simonDef = diceRoll(simon.agility);
+                        // monster attacks
+                        System.out.println("You faild to escape. The " + monsterObj.creatureIsA + " attacks!");
+                        boolean fight = true;
+                        while (fight) {
+                            int monsterAtk = diceRoll(monsterObj.attack);
+                            int simonDef = diceRoll(simon.agility);
 
-                        System.out.println("The " + monsterObj.creatureIsA + " attacks you for " + monsterAtk + " damage!");
-                        System.out.println("You defend yourself for " + simonDef);
-                        if (monsterAtk > simonDef) {
+                            System.out.println("The " + monsterObj.creatureIsA + " attacks you for " + monsterAtk + " damage!");
+                            System.out.println("You defend yourself for " + simonDef);
+                            if (monsterAtk > simonDef) {
 
-                            System.out.println("You took damage! You had " + simon.toughness + " toughness");
-                            simon.toughness--;
-                            System.out.println("But now you have " + simon.toughness);
+                                System.out.println("You took damage! You had " + simon.toughness + " toughness");
+                                simon.toughness--;
+                                System.out.println("But now you have " + simon.toughness);
 
-                        } else if (monsterAtk < simonDef) {
-                            System.out.println("You defended yourself from the attack!");
-                        } else if (monsterAtk == simonDef) {
-                            System.out.println("Draw!");
+                            } else if (monsterAtk < simonDef) {
+                                System.out.println("You defended yourself from the attack!");
+                            } else if (monsterAtk == simonDef) {
+                                System.out.println("Draw!");
+                            }
+                            if (simon.toughness == 0) {
+                                System.out.println("----------------------------");
+                                System.out.println("Simon the brave knight died!");
+                                System.out.println("----------------------------");
+                                fight = false;
+                                break;
+                            }
+
+                            monsterObj.initiative--;
+                            if (monsterObj.initiative == 0) {
+                                System.out.println("------------");
+                                System.out.println("You made it!");
+                                System.out.println("------------");
+                                fight = false;
+                            }
+
                         }
-                        if (simon.toughness == 0 ) {
-                            System.out.println("----------------------------");
-                            System.out.println("Simon the brave knight died!");
-                            System.out.println("----------------------------");
-                            fight = false;
-                            break;
-                        }
-                        
-                        monsterObj.initiative--;                        
-                        if (monsterObj.initiative==0){
-                            System.out.println("------------");
-                            System.out.println("You made it!");
-                            System.out.println("------------");
-                            fight = false;
-                        }                       
-                                                
                     }
-                }
 
-                //if input is neither 1 or 0, inform user of invalid format
-                else {
+                    //if input is neither 1 or 0, inform user of invalid format
+
+                }
+                else{
                     System.out.print("Incorrect input, enter '1' or '0'. You wrote: " + mainInput + "\n");
                 }
+                } catch(Exception e){
+                    //Any corrupted or invalid input results in a exception. Inform user that the input is invalid
+                    System.out.print("Invalid input. Must be integers...\n");
+                    System.out.println(e.getMessage() != null ? e.getMessage() : "");
+                    input.next();
+                    programRunner = true;
+                }
 
-            } catch (Exception e) {
-                //Any corrupted or invalid input results in a exception. Inform user that the input is invalid
-                System.out.print("Invalid input. Must be integers...\n");
-                System.out.println(e.getMessage() != null ? e.getMessage() : "");
-                input.next();
-                programRunner = true;
-            }
         }
 
     }
