5d5ae31c4fb1f1bd8b37bf4cdca81c691fe75fc5
Ludde <Ludde@LAPTOP-C2OOO8UV.lan>
2020-11-09 10:40:39 +0100

    Thief med string name

--- src/dungeonrun/Characters/Thief.java
+++ src/dungeonrun/Characters/Thief.java
@@ -2,16 +2,17 @@ package dungeonrun.Characters;
 
 import dungeonrun.Monsters.Creatures;
 
-public class Thief extends Creatures {
-    
-    public Thief(){
-        creatureIsA = "Theif";
+public class Thief extends Heroes {
 
+    public Thief(String playersName) {
+        super(playersName);
         initiative = 7;
         toughness = 5;
         attack = 5;
         agility = 7;
     }
+    
+    
     public void specialAbility(){
         //"Critical hit". Theif has 25% chance to do double damage every time Thief attacks.
     }
