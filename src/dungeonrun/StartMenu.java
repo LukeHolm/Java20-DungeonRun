f0fd866e8242efeef7f33b5dd45a687d7a2aa4a9
Ludde <Ludde@LAPTOP-C2OOO8UV.lan>
2020-11-09 11:51:53 +0100

    Method "characters" bytt namn till Player.

--- src/dungeonrun/StartMenu.java
+++ src/dungeonrun/StartMenu.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StartMenu {
+
     ArrayList<Heroes> player = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
     static int choice;
@@ -15,7 +16,7 @@ public class StartMenu {
     static String characterName;
     static int Hero;
     MapMenu play = new MapMenu();
-    
+
     Map startMenuMap = null;
 
     public void main() {
@@ -27,7 +28,7 @@ public class StartMenu {
 
         switch (choice) {
             case 1:
-                characters();
+                Player();
                 startMenuMap = play.chooseMap();
                 break;
             case 2:
@@ -41,11 +42,11 @@ public class StartMenu {
 
             default:
                 System.out.println("Invalid input, please try again.");
-
         }
     }
-    public void characters() {
-        
+
+    public void Player() {
+
         System.out.println("\nPlease choose a character\n");
         System.out.println("-----------------------------------\n ");
         System.out.println("\033[0;1m 1. Knight\033[0m\n Initiativ: 5\n Tålighet: 9\n Attack 6\n Smidighet: 4\n ");
@@ -55,20 +56,20 @@ public class StartMenu {
         System.out.println("\033[0;1m 3. Thief\033[0m\n Initiativ: 7\n Tålighet: 5\n Attack 5\n Smidighet: 7\n");
         System.out.println("-----------------------------------\n");
         Hero = scanner.nextInt();
+        scanner.nextLine();
         switch (Hero) {
             case 1:
                 System.out.println("Enter the name of your character");
                 characterName = scanner.nextLine();
-                
+
                 System.out.println("\nYou have chosen the brave knight" + " " + characterName);
                 Heroes playerHero = new Knight(characterName);
                 player.add(playerHero);
-
                 break;
             case 2:
                 System.out.println("\nEnter the name of your character");
                 characterName = scanner.nextLine();
-                
+
                 System.out.println("\nYou have chosen the intelligent wizard" + " " + characterName);
                 Heroes playerHeroWizard = new Wizard(characterName);
                 player.add(playerHeroWizard);
@@ -76,7 +77,7 @@ public class StartMenu {
             case 3:
                 System.out.println("Enter the name of your character");
                 characterName = scanner.nextLine();
-                
+
                 System.out.println("\nYou have chosen the cunning thief" + " " + characterName);
                 Heroes playerHeroThief = new Thief(characterName);
                 player.add(playerHeroThief);
