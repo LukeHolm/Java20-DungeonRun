71dbf43d2bfd506bb70f03863a79e45c53415733
lucas <lucas@LAPTOP-U93GLV2V.mynet>
2020-11-11 10:01:43 +0100

    Updated text in loose coins

--- src/dungeonrun/Treasures/LooseCoins.java
+++ src/dungeonrun/Treasures/LooseCoins.java
@@ -2,7 +2,7 @@ package dungeonrun.Treasures;
 
 public class LooseCoins extends Treasure {
     public LooseCoins(){
-        treasureIsA = "some loose coins";
+        treasureIsA = "loose coins";
         value = 2;
         frequency = 0.4;
     }
