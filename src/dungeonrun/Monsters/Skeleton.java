127d9723431770762286abc6488f3d86ef249472
cristina <admin@mac-00196.local>
2020-11-13 19:15:59 +0100

    update skeleton ascii

--- src/dungeonrun/Monsters/Skeleton.java
+++ src/dungeonrun/Monsters/Skeleton.java
@@ -18,4 +18,37 @@ public class Skeleton extends Monster {
     public String toString() {
         return "Skeleton";
     }
+    
+    @Override
+    public String[] displayStrings() {
+        // Here you can use "£" instead of "\\" so the rows lines up, and it will be replaced with "\" in the code below 
+        String[] ascii = {
+          		"                            ", 
+         		"                            ",
+        		"          .-.               ",
+        		"         (o.o)              ",
+        		"          |=|               ", //row 5
+        		"           |                ",        		
+        		"         __|__              ",
+        		"       //.=|=.££            ",
+        		"      // .=|=. ££           ",
+        		"      ££ .=|=. //           ", //row 10
+        		"       ££(_=_)//            ",
+        		"        (:| |:)             ",
+        		"         || ||              ",
+        		"         || ||              ",        		
+        		"         () ()              ", //row 15
+        		"         || ||              ",  
+        		"         || ||              ",        		      		
+        		"         || ||              ",
+        		"        ==' '==             ",
+        		"                            ",//row 20
+        		"                            "};
+            //   1234567890123456789012345678
+        // Replace "£" with "\"
+        for (int i = 0; i < ascii.length; i++) {
+            ascii[i] = ascii[i].replace("£", "\\");
+        }
+        return ascii;
+    }
 }
