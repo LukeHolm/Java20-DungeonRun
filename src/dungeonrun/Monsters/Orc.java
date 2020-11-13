3ba46c439e9c3db8265c3222cff5bffb26f5ad1f
cristina <admin@mac-00196.local>
2020-11-13 19:40:18 +0100

    update Orc ascii

--- src/dungeonrun/Monsters/Orc.java
+++ src/dungeonrun/Monsters/Orc.java
@@ -19,4 +19,38 @@ public class Orc extends Monster {
     public String toString() {
         return "Orc";
     }
+    
+    @Override
+    public String[] displayStrings() {
+        // Here you can use "£" instead of "\\" so the rows lines up, and it will be replaced with "\" in the code below 
+        String[] ascii = {
+        		   "                            ",
+        		   "  /|                        ",
+        		   "  ||                        ",
+        		   "  ||        |       |       ",                           
+        		   "  ||        |       |       ",//row 5
+        		   "  ||      ,/  _____  £.     ",    
+        		   "  ||      ||_/     £_||     ",        
+        		   "  ||       £_| . . |_/      ",        
+        		   "  ||         |  L  |        ",        
+        		   " ,||         |`==='|        ", //row 10        
+        		   " |>|      ___`>  -<'___     ",        
+        		   " |>|£    /             £    ",        
+        		   " £>| £  /  ,    .    .  |   ",        
+        		   "  ||  £/  /| .  |  . |  |   ",        
+        		   "  ||£  ` / | ___|___ |  |   ",       
+        		   "  || `--'  | _______ |  |   ",//row 15   
+        		   "  || (  )£ | - --- - | -| ( ", 
+        		   "  || ))/ ( | -- - -- |  | ))", 
+        		   "()||((( ())|         |  |( (", 
+        		   "                            ",
+        		   "                            ",//row 20
+        		   "                            "};
+               //   1234567890123456789012345678
+        // Replace "£" with "\"
+        for (int i = 0; i < ascii.length; i++) {
+            ascii[i] = ascii[i].replace("£", "\\");
+        }
+        return ascii;
+    }
 }
