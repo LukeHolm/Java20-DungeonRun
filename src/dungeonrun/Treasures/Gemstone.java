d46950d741cf1b0beb5380c8da903ffba7065a96
Greger Lindkvist <greg.kvist@gmail.com>
2020-11-13 09:25:26 +0100

    Added Gemstone ascii.

--- src/dungeonrun/Treasures/Gemstone.java
+++ src/dungeonrun/Treasures/Gemstone.java
@@ -10,11 +10,32 @@ package dungeonrun.Treasures;
  * @author lucas
  */
 public class Gemstone extends Treasure {
-    public Gemstone(){
+
+    public Gemstone() {
         treasureDescription = "a shiny gemstone";
         name = "gemstone";
         value = 14;
         frequency = 0.1;
     }
-    
+    // Returning a String array of the treaasiures looks, so characters can be written row for row, with other things further away on each row.
+
+    @Override
+    public String[] displayStrings() {
+        // Here you can use "£" instead of"\\" so the rows lines up, and it will be replaced with "\" in the code below 
+        String[] ascii = {
+            "   £  |  |  |  /  ",
+            " £ ____________  /", // 2
+            "  /£  /£  /£  /£  ",
+            " / .£/..£/..£/ .£ ", // 4
+            "  £  £ |  | /  /  ",
+            "    £ £ || / /    ", // 6
+            "      ££||//      ",
+            "        £/        "}; // 8
+        //   123456789012345678
+        // Replace "£" with "\"
+        for (int i = 0; i < ascii.length; i++) {
+            ascii[i] = ascii[i].replace("£", "\\");
+        }
+        return ascii;
+    }
 }
