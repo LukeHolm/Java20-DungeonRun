08593f1a9b84061cd46b551efe2aac2f629c0b33
Greger Lindkvist <greg.kvist@gmail.com>
2020-11-16 02:38:52 +0100

    Added colorLookup[] in Map for adding color to displayStrings(), and tried it out in Wizard.

--- src/dungeonrun/Characters/Wizard.java
+++ src/dungeonrun/Characters/Wizard.java
@@ -1,5 +1,7 @@
 package dungeonrun.Characters;
 
+import dungeonrun.Map;
+
 public class Wizard extends Heroes {
 
     public Wizard(String playersName) {
@@ -20,7 +22,6 @@ public class Wizard extends Heroes {
         return "Wizard name is " + playersName + "\n";
     }
 
-  
     @Override
     public String[] displayStrings() {
         // Characters like '\' and '" 'messes upp the line lengths, since you need to write \\ and \" to get them in a string. 
@@ -48,12 +49,53 @@ public class Wizard extends Heroes {
             " _.'   /     db     '.  '._ ", // Row 20
             "(___rf£____.dBBb._____)____)"};
         //   1234567890123456789012345678
+        String[] color = {
+            "               _____        ",
+            "             .'y *g=yg      ",
+            "          __/_ygyg_  ££     ",
+            "         / _______ £  y     ",
+            "        _£_)/___£(_/_       ", // Row 5
+            "       / _w(yb byw)g £      ",
+            "       £ £w))(sw(()g /      ",
+            "        ' £w((()))g '       ",
+            "       / ' £w).))g ' £      ",
+            "      / _ £ - | - /_  £     ", // Row 10
+            "     (   ( m;''';. g'  )    ",
+            "     _£§__ m    )£ g_§/_    ",
+            "       £/  m   ' /  g/      ",
+            "        .'  m...' g )       ",
+            "         /  / | £  £        ", // Row 15
+            "        /  .  .  .  £       ",
+            "       /   .     .   £      ",
+            "      /   /   |   £   £     ",
+            "    .'   /    B    '.  '.   ",
+            " _.'   /     DB    '.   '._ ", // Row 20
+            "(___RF£____.DBBB___ __)____)"};
+        //   1234567890123456789012345678
+        
+        String str;
+        String[] coloredAscii = new String[ascii.length];
+        char chr;
+        for (int row = 0; row < ascii.length; row++) {
+            // Replace '£' with '\' and replace '§' with '"'
+            ascii[row] = ascii[row].replace("£", "\\");
+            ascii[row] = ascii[row].replace("§", "\"");
 
-        // Replace '£' with '\' and replace '§' with '"'
-        for (int i = 0; i < ascii.length; i++) {
-            ascii[i] = ascii[i].replace("£", "\\");
-            ascii[i] = ascii[i].replace("§", "\"");
+            str = "";
+            for (int x = 0; x < ascii[row].length(); x++) {
+
+                // Get the corresponding location in the color strings, and check if it's lower case letter
+                chr = color[row].charAt(x);
+
+                if (Character.isLowerCase(chr)) {
+                    //  Lookup the corresponding color code, and add it to the output string
+                    str += Map.colorLookup[(int) (chr - 'a')];
+                }
+                // Always add the chracter from the ascii strings
+                str += ascii[row].charAt(x);
+            }
+            coloredAscii[row] = str;
         }
-        return ascii;
+        return coloredAscii;
     }
 }
--- src/dungeonrun/Map.java
+++ src/dungeonrun/Map.java
@@ -31,6 +31,7 @@ public class Map {
     public static final String WHITE = "\u001b[37m";
     // Bright versions:
     public static final String BR_BLACK = "\u001b[30;1m";
+    public static final String GREY = BR_BLACK;
     public static final String BR_RED = "\u001b[31;1m";
     public static final String BR_GREEN = "\u001b[32;1m";
     public static final String BR_YELLOW = "\u001b[33;1m";
@@ -38,6 +39,34 @@ public class Map {
     public static final String BR_MAGENTA = "\u001b[35;1m";
     public static final String BR_CYAN = "\u001b[36;1m";
     public static final String BR_WHITE = "\u001b[37;1m";
+    // Dark versions:
+    public static final String D_BLACK = "\u001b[30;2m";
+    public static final String D_RED = "\u001b[31;2m";
+    public static final String D_GREEN = "\u001b[32;2m";
+    public static final String D_YELLOW = "\u001b[33;2m";
+    public static final String D_BLUE = "\u001b[34;2m";
+    public static final String D_MAGENTA = "\u001b[35;2m";
+    public static final String D_CYAN = "\u001b[36;2m";
+    public static final String D_WHITE = "\u001b[37;2m";
+    // Extra dark(?) version
+    public static final String DD_BLACK = "\u001b[30;3m";
+    public static final String DD_RED = "\u001b[31;3m";
+    public static final String DD_GREEN = "\u001b[32;3m";
+    public static final String DD_YELLOW = "\u001b[33;3m";
+    public static final String DD_BLUE = "\u001b[34;3m";
+    public static final String DD_MAGENTA = "\u001b[35;3m";
+    public static final String DD_CYAN = "\u001b[36;3m";
+    public static final String DD_WHITE = "\u001b[37;3m";
+
+    // Mapping lower case letters to color code strings, index with "(int)(char - 'a')"
+    public static final String[] colorLookup = {
+        DD_BLUE/*a*/, BR_BLUE/*b*/, BR_CYAN/*c*/, DD_CYAN/*d*/,
+        BR_BLACK/*e = grey*/, DD_WHITE/*f*/, BR_GREEN/*g*/, DD_GREEN/*h*/,
+        BLINK/*i*/, ""/*j*/, BR_BLACK/*k*/, ""/*l*/,
+        BR_MAGENTA/*m*/, DD_MAGENTA/*n*/, ""/*o*/, ""/*p*/,
+        RESET/*q*/, BR_RED/*r*/, DD_RED /*s*/, REVERSE_VIDEO/*t*/,
+        UNDERLINE/*u*/, BR_WHITE/*v*/, RESET/*w = white*/, DD_WHITE/*x*/,
+        BR_YELLOW/*y*/, DD_YELLOW/*z*/};
 
     Scanner input = new Scanner(System.in);
     public Room[][] rooms;
